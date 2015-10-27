d9f4e1f8aa02765992b436227c3cadb1293da821
lincircle <cosine@cosin.local>
2015-10-27 20:19:48 +0800

    Initial Commit

---
+++ 1027projectTests/_027projectTests.swift
@@ -0,0 +1,36 @@
+//
+//  _027projectTests.swift
+//  1027projectTests
+//
+//  Created by Lin Yu-Hsuan on 2015/10/27.
+//  Copyright © 2015年 Lin Circle. All rights reserved.
+//
+
+import XCTest
+@testable import _027project
+
+class _027projectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
